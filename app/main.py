c6b0b7c5590d04ed9a872c8504cf0a30ada62546
lorenekeda <lorenekeda@gmail.com>
2026-06-13 15:37:52 -0400

    jinja template trial

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def home():
-    return {'message': "Opportunity Aggregator API"}
+
+@app.get("/", include_in_schema=False)
+def home(request: Request):
+    return templates.TemplateResponse(request, "home.html")
